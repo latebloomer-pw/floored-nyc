b6bb1515f822eda8546e232b669c5531e56982fb
pete <pete@latebloomer.studio>
2025-02-04 11:29:19 -0800

    add spam messaging

--- src/app/auth/page.tsx
+++ src/app/auth/page.tsx
@@ -6,7 +6,7 @@ export default function AuthPage() {
     return (
         <div className="container mx-auto py-8 my-20">
             <h1 className="text-2xl font-bold text-center mb-8">Sign In / Sign Up</h1>
-            <p className="text-center mb-8">Please check your spam folder if you don&apos;t see an email from us.</p>
+            <p className="text-center mb-8">Please check your spam folder for your verification email.</p>
             <Auth />
         </div>
     );
